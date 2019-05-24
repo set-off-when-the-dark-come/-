a6176a0b1f5ef98f335bf9f17d0c9fb012e8f9a2
lgs474131498 <40853089+lgs474131498@users.noreply.github.com>
2019-05-24 22:04:55 +0800

    app.js的改变
    app.jshe config.json

--- 6.小程序/app.js
+++ 6.小程序/app.js
@@ -2,24 +2,19 @@
 App({
   onLaunch: function () {
     // 展示本地存储能力
-    console.log("Onluanch!");
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
     // 登录
     wx.login({
-     
-     url:"https://",
-     success: res => {
-       console.log("log in!");
+      success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
     // 获取用户信息
     wx.getSetting({
       success: res => {
-        console.log("getSetting");
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
@@ -35,10 +30,6 @@ App({
             }
           })
         }
-      },
-      fail:res=>
-      {
-        console.log("fail!");
       }
     })
   },
